db32f42257a8961f270f5c42f1c6598a81e41012
GauravSanchania <sanchania2042@gmail.com>
2024-09-29 23:06:24 +0530

    added display container to display output, script changes

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 const citySearchInput = document.getElementById('city-search');
 const searchButton = document.getElementById('search-button');
 const apiKey = 'd6d8982cca5247a882a132815242909'; // Weather API key
+const weatherDisplay = document.getElementById('weather-display');
 
 searchButton.addEventListener("click", () => {
   const cityName = citySearchInput.value;
@@ -18,8 +19,20 @@ function fetchWeatherData(cityName) {
       .then(response => response.json())
       .then(data => {
         console.log(data);
+        displayWeatherData(data);
       })
       .catch(error => {
         console.error('Error fetching weather data:', error);
       });
+}
+
+function displayWeatherData(data) {
+    const weatherInfo = `
+      <h2>${data.location.name}, ${data.location.country}</h2>
+      <p>Temperature: ${data.current.temp_c}°C</p>
+      <p>Condition: ${data.current.condition.text}</p>
+      <img src="${data.current.condition.icon}"
+      alt="${data.current.condition.text}">
+      `;
+    weatherDisplay.innerHTML = weatherInfo;
 }
