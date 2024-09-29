60c296ee8b04161a4eb3a22d3fca2360609a9a36
GauravSanchania <sanchania2042@gmail.com>
2024-09-30 00:16:08 +0530

    added local storage for recent searched cities

--- src/script.js
+++ src/script.js
@@ -3,14 +3,37 @@ const searchButton = document.getElementById('search-button');
 const apiKey = 'd6d8982cca5247a882a132815242909'; // Weather API key
 const weatherDisplay = document.getElementById('weather-display');
 const currentLocationButton = document.getElementById('current-location-button');
+const dropdownMenu = document.getElementById('dropdown-menu');
+
+function updateDropdownMenu() {
+    const recentlySearchedCities = JSON.parse(localStorage.getItem('recentlySearchedCities')) || [];
+    dropdownMenu.innerHTML = ''; 
+
+    recentlySearchedCities.forEach((city) => {
+    const dropdownItem = document.createElement('li');
+    dropdownItem.textContent = city;
+    dropdownItem.addEventListener('click', () => {
+        citySearchInput.value = city;
+        fetchWeatherData(city);
+        dropdownMenu.classList.add('hidden');
+    });
+    dropdownMenu.appendChild(dropdownItem);
+  });
+}
 
 searchButton.addEventListener("click", () => {
     currentLocationButton.disabled = true;
     currentLocationButton.style.backgroundColor = 'grey';
-    
+
     const cityName = citySearchInput.value;
+    const cityNameRegex = /^[a-zA-Z\s]+$/;
     if (cityName) {
+      if (!cityNameRegex.test(cityName)) {
+        alert('Please enter a valid city name.');
+        return;
+      }
       fetchWeatherData(cityName);
+      dropdownMenu.classList.add('hidden');
     } else {
       alert('Please enter a city name.');
     }
@@ -28,6 +51,13 @@ function fetchWeatherData(cityName) {
       .catch(error => {
         console.error('Error fetching weather data:', error);
       });
+
+      const recentlySearchedCities = JSON.parse(localStorage.getItem('recentlySearchedCities')) || [];
+      if (!recentlySearchedCities.includes(cityName)) {
+        recentlySearchedCities.unshift(cityName);
+        localStorage.setItem('recentlySearchedCities', JSON.stringify(recentlySearchedCities));
+      }
+      updateDropdownMenu();
 }
 
 function displayWeatherData(data) {
@@ -40,3 +70,6 @@ function displayWeatherData(data) {
       `;
     weatherDisplay.innerHTML = weatherInfo;
 }
+
+updateDropdownMenu();
+localStorage.clear();
